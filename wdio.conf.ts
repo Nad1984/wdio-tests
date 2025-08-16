9e5354c5996953b2926122869ad3cb6b08fbb194
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2025-08-16 22:48:54 +0300

    Revert "Merge branch 'main' of https://github.com/Nad1984/wdio-tests"
    This reverts commit 19c93d4b1a8a87cbd6a3ec3ddfe15f9110a6002d, reversing
    changes made to 3fcddca2d8e300f0965c8499e81ab7864db0c4de.

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,20 +1,58 @@
-import { baseConfig } from './wdio.base.conf';
-
-const isCI = process.env.CI === 'true';
-
+import "@wdio/allure-reporter";
 export const config: WebdriverIO.Config = {
-  ...baseConfig,
-  capabilities: [
-    ...baseConfig.capabilities!,
-    {
-      browserName: 'firefox',
-      'moz:firefoxOptions': { args: ['-headless'] },
-    },
-    ...(!isCI ? [{ browserName: 'safari' }] : []),
+  runner: "local",
+  tsConfigPath: "./test/tsconfig.json",
+
+  specs: ["./test/specs/**/*.ts"],
+
+  exclude: [
   ],
-  ...(isCI && {
-    hostname: 'selenium-hub',
-    port: 4444,
-    path: '/wd/hub',
-  }),
+
+  maxInstances: 10,
+  baseUrl: 'https://www.saucedemo.com/',
+  capabilities: [
+    {
+      browserName: "chrome",
+      "goog:chromeOptions": {
+        args: ["headless", "disable-gpu"],
+      },
+    },
+    {
+      browserName: "firefox",
+      "moz:firefoxOptions": {
+        args: ["-headless"],
+      },
+    },
+    {
+      browserName: "safari",
+    },
+  ],
+
+  logLevel: "info",
+  bail: 0,
+
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: ["visual"],
+
+  framework: "mocha",
+  reporters: [
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: false,
+        disableWebdriverScreenshotsReporting: false,
+      },
+    ],
+  ],
+  beforeSession: function () {
+    require("ts-node").register({ files: true });
+  },
+
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
 };
