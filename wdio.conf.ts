d10feba2cf2c0232dd61b14256b94c1f2c35a434
Nad1984 <71829239+Nad1984@users.noreply.github.com>
2025-08-16 21:00:36 +0300

    fix tests

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,50 +1,26 @@
-import "@wdio/allure-reporter";
-import { config as baseConfig } from "./wdio.conf";
+import { baseConfig } from "./wdio.base.conf";
 
-const isCI = process.env.CI === "true"; // GitHub Actions sets this to true
-
+const isCI = process.env.CI === "true";
 export const config: WebdriverIO.Config = {
   ...baseConfig,
-  runner: "local",
-  specs: ["./test/specs/**/*.ts"],
+  
+  // Override or augment capabilities
   capabilities: [
-    {
-      browserName: "chrome",
-      "goog:chromeOptions": {
-        args: ["--headless", "--disable-gpu", "--no-sandbox", "--disable-dev-shm-usage"],
-      },
-    },
+    ...baseConfig.capabilities,
     {
       browserName: "firefox",
       "moz:firefoxOptions": {
         args: ["-headless"],
       },
     },
-    ...(isCI
-      ? []
-      : [
-          {
-            browserName: "safari",
-          },
-        ]),
+    // Only run Safari locally
+    ...(!isCI ? [{ browserName: "safari" }] : []),
   ],
-  services: [],
-  framework: "mocha",
-  reporters: [
-    [
-      "allure",
-      {
-        outputDir: "allure-results",
-        disableWebdriverStepsReporting: false,
-        disableWebdriverScreenshotsReporting: false,
-      },
-    ],
-  ],
-  beforeSession: function () {
-    require("ts-node").register({ files: true });
-  },
-  mochaOpts: {
-    ui: "bdd",
-    timeout: 60000,
-  },
+
+  // Add Selenium Grid config when in GitHub Actions
+  ...(isCI && {
+    hostname: "selenium-hub",
+    port: 4444,
+    path: "/wd/hub",
+  }),
 };
